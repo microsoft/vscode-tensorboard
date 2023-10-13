c45a2c7b663d7ca4ec1193745aa9dded12778324
Don Jayamanne <don.jayamanne@outlook.com>
2023-10-13 13:03:57 +1100

    Single code lens provider instead of multiples

--- src/codelensProvider.ts
+++ src/codelensProvider.ts
@@ -1,80 +1,50 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { CancellationToken, CodeLens, Command, Disposable, languages, Position, Range, TextDocument } from 'vscode';
-import {
-    Commands,
-    NotebookCellScheme,
-    PYTHON,
-    PYTHON_LANGUAGE,
-    TensorBoardEntrypoint,
-    TensorBoardEntrypointTrigger
-} from './constants';
-import { containsNotebookExtension } from './helpers';
+import { CodeLens, Command, languages, Position, Range } from 'vscode';
+import { Commands, PYTHON, TensorBoardEntrypoint, TensorBoardEntrypointTrigger } from './constants';
+import { containsNotebookExtension, containsTensorBoardImport } from './helpers';
 import { Localized } from './common/localize';
 import { sendTensorboardEntrypointTriggered } from './common/telemetry';
 import { once } from './common/functional';
 
-export function registerCodeLensProvider() {
-    const disposable1 = languages.registerCodeLensProvider(
-        [
-            { scheme: NotebookCellScheme, language: PYTHON_LANGUAGE },
-            { scheme: 'vscode-notebook', language: PYTHON_LANGUAGE }
-        ],
-        {
-            provideCodeLenses: (document, token) =>
-                provideCodeLenses(
-                    TensorBoardEntrypointTrigger.nbextension,
-                    document,
-                    token,
-                    once(() =>
-                        sendTensorboardEntrypointTriggered(
-                            TensorBoardEntrypointTrigger.nbextension,
-                            TensorBoardEntrypoint.codelens
-                        )
-                    )
-                )
-        }
-    );
-    const disdposable2 = languages.registerCodeLensProvider(PYTHON, {
-        provideCodeLenses: (document, token) =>
-            provideCodeLenses(
-                TensorBoardEntrypointTrigger.fileimport,
-                document,
-                token,
-                once(() =>
-                    sendTensorboardEntrypointTriggered(
-                        TensorBoardEntrypointTrigger.fileimport,
-                        TensorBoardEntrypoint.codelens
-                    )
-                )
-            )
-    });
-    return Disposable.from(disposable1, disdposable2);
-}
+const sendNotebookTelemetry = once(() =>
+    sendTensorboardEntrypointTriggered(TensorBoardEntrypointTrigger.nbextension, TensorBoardEntrypoint.codelens)
+);
+const sendPythonTelemetry = once(() =>
+    sendTensorboardEntrypointTriggered(TensorBoardEntrypointTrigger.fileimport, TensorBoardEntrypoint.codelens)
+);
 
-function provideCodeLenses(
-    trigger: TensorBoardEntrypointTrigger,
-    document: TextDocument,
-    cancelToken: CancellationToken,
-    sendTelemetry: (trigger: TensorBoardEntrypointTrigger) => void
-): CodeLens[] {
-    const command: Command = {
-        title: Localized.launchNativeTensorBoardSessionCodeLens,
-        command: Commands.LaunchTensorBoard,
-        arguments: [{ trigger, entrypoint: TensorBoardEntrypoint.codelens }]
-    };
-    const codelenses: CodeLens[] = [];
-    for (let index = 0; index < document.lineCount; index += 1) {
-        if (cancelToken.isCancellationRequested) {
+export function registerCodeLensProvider() {
+    return languages.registerCodeLensProvider(PYTHON, {
+        provideCodeLenses: (document, token) => {
+            const codelenses: CodeLens[] = [];
+            for (let index = 0; index < document.lineCount; index += 1) {
+                if (token.isCancellationRequested) {
+                    return codelenses;
+                }
+                const { lineNumber, text } = document.lineAt(index);
+                const trigger = containsNotebookExtension([text])
+                    ? TensorBoardEntrypointTrigger.nbextension
+                    : containsTensorBoardImport([text])
+                    ? TensorBoardEntrypointTrigger.fileimport
+                    : undefined;
+                if (trigger) {
+                    const command: Command = {
+                        title: Localized.launchNativeTensorBoardSessionCodeLens,
+                        command: Commands.LaunchTensorBoard,
+                        arguments: [{ trigger, entrypoint: TensorBoardEntrypoint.codelens }]
+                    };
+                    const range = new Range(new Position(lineNumber, 0), new Position(lineNumber, 1));
+                    codelenses.push(new CodeLens(range, command));
+                    if (trigger === TensorBoardEntrypointTrigger.nbextension) {
+                        sendNotebookTelemetry();
+                    } else {
+                        sendPythonTelemetry();
+                    }
+                }
+            }
             return codelenses;
         }
-        const line = document.lineAt(index);
-        if (containsNotebookExtension([line.text])) {
-            const range = new Range(new Position(line.lineNumber, 0), new Position(line.lineNumber, 1));
-            codelenses.push(new CodeLens(range, command));
-            sendTelemetry(trigger);
-        }
-    }
-    return codelenses;
+    });
 }
