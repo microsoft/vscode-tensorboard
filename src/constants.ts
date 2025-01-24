0288a05842106a80fdf580a20a7b85b92409f08a
Logan Ramos <lramos15@gmail.com>
2025-01-24 06:40:48 -0700

    Use strings instead of enum for event name (#32)

--- src/constants.ts
+++ src/constants.ts
@@ -4,37 +4,28 @@
 import { ExtensionContext } from 'vscode';
 
 export enum TensorBoardPromptSelection {
-    Yes = 'yes',
-    No = 'no',
-    DoNotAskAgain = 'doNotAskAgain',
-    None = 'none'
+  Yes = 'yes',
+  No = 'no',
+  DoNotAskAgain = 'doNotAskAgain',
+  None = 'none',
 }
 
 export enum TensorBoardEntrypointTrigger {
-    tfeventfiles = 'tfeventfiles',
-    fileimport = 'fileimport',
-    nbextension = 'nbextension',
-    palette = 'palette'
+  tfeventfiles = 'tfeventfiles',
+  fileimport = 'fileimport',
+  nbextension = 'nbextension',
+  palette = 'palette',
 }
 
 export enum TensorBoardSessionStartResult {
-    cancel = 'canceled',
-    success = 'success',
-    error = 'error'
+  cancel = 'canceled',
+  success = 'success',
+  error = 'error',
 }
 
 export namespace Commands {
-    export const LaunchTensorBoard = 'tensorboard.launch';
-    export const RefreshTensorBoard = 'tensorboard.refresh';
-}
-
-export enum EventName {
-    TENSORBOARD_SESSION_LAUNCH = 'TENSORBOARD.SESSION_LAUNCH',
-    TENSORBOARD_SESSION_DAEMON_STARTUP_DURATION = 'TENSORBOARD.SESSION_DAEMON_STARTUP_DURATION',
-    TENSORBOARD_ENTRYPOINT_SHOWN = 'TENSORBOARD.ENTRYPOINT_SHOWN',
-    TENSORBOARD_INSTALL_PROMPT_SELECTION = 'TENSORBOARD.INSTALL_PROMPT_SELECTION',
-    TENSORBOARD_JUMP_TO_SOURCE_REQUEST = 'TENSORBOARD_JUMP_TO_SOURCE_REQUEST',
-    TENSORBOARD_JUMP_TO_SOURCE_FILE_NOT_FOUND = 'TENSORBOARD_JUMP_TO_SOURCE_FILE_NOT_FOUND'
+  export const LaunchTensorBoard = 'tensorboard.launch';
+  export const RefreshTensorBoard = 'tensorboard.refresh';
 }
 
 export const PYTHON_LANGUAGE = 'python';
@@ -44,21 +35,21 @@ export const InteractiveInputScheme = 'vscode-interactive-input';
 export const PythonExtensionId = 'ms-python.python';
 
 export enum TensorBoardEntrypoint {
-    prompt = 'prompt',
-    codelens = 'codelens',
-    palette = 'palette'
+  prompt = 'prompt',
+  codelens = 'codelens',
+  palette = 'palette',
 }
 
 export const PYTHON = [
-    { scheme: 'file', language: PYTHON_LANGUAGE },
-    { scheme: 'untitled', language: PYTHON_LANGUAGE },
-    { scheme: 'vscode-notebook', language: PYTHON_LANGUAGE },
-    { scheme: NotebookCellScheme, language: PYTHON_LANGUAGE },
-    { scheme: InteractiveInputScheme, language: PYTHON_LANGUAGE }
+  { scheme: 'file', language: PYTHON_LANGUAGE },
+  { scheme: 'untitled', language: PYTHON_LANGUAGE },
+  { scheme: 'vscode-notebook', language: PYTHON_LANGUAGE },
+  { scheme: NotebookCellScheme, language: PYTHON_LANGUAGE },
+  { scheme: InteractiveInputScheme, language: PYTHON_LANGUAGE },
 ];
 
 export const ExtensionInfo: {
-    context: ExtensionContext;
-    // Value will be set in extension.ts
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  context: ExtensionContext;
+  // Value will be set in extension.ts
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
 } = {} as any;
