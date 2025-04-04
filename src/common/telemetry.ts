32b6a9886ccf06ccad7552e09e7f7c42c80c2818
Don Jayamanne <don.jayamanne@outlook.com>
2025-04-04 15:01:51 +1100

    Updates

--- src/common/telemetry.ts
+++ src/common/telemetry.ts
@@ -6,10 +6,10 @@ import { AppInsightsKey } from './constants';
 import { disposableStore } from './lifecycle';
 import { computeHash } from './crypto';
 import {
-  TensorBoardEntrypoint,
-  TensorBoardEntrypointTrigger,
-  TensorBoardPromptSelection,
-  TensorBoardSessionStartResult,
+    TensorBoardEntrypoint,
+    TensorBoardEntrypointTrigger,
+    TensorBoardPromptSelection,
+    TensorBoardSessionStartResult
 } from '../constants';
 
 // #region Telemetry
@@ -17,47 +17,47 @@ import {
 // #endregion
 
 export interface IPropertyData {
-  classification:
-    | 'SystemMetaData'
-    | 'CallstackOrException'
-    | 'CustomerContent'
-    | 'PublicNonPersonalData'
-    | 'EndUserPseudonymizedInformation';
-  purpose: 'PerformanceAndHealth' | 'FeatureInsight' | 'BusinessInsight';
-  comment: string;
-  expiration?: string;
-  endpoint?: string;
-  isMeasurement?: boolean;
+    classification:
+        | 'SystemMetaData'
+        | 'CallstackOrException'
+        | 'CustomerContent'
+        | 'PublicNonPersonalData'
+        | 'EndUserPseudonymizedInformation';
+    purpose: 'PerformanceAndHealth' | 'FeatureInsight' | 'BusinessInsight';
+    comment: string;
+    expiration?: string;
+    endpoint?: string;
+    isMeasurement?: boolean;
 }
 
 export interface IGDPRProperty {
-  owner: string;
-  comment: string;
-  expiration?: string;
-  readonly [name: string]: IPropertyData | undefined | IGDPRProperty | string;
+    owner: string;
+    comment: string;
+    expiration?: string;
+    readonly [name: string]: IPropertyData | undefined | IGDPRProperty | string;
 }
 
 type IGDPRPropertyWithoutMetadata = Omit<IGDPRProperty, 'owner' | 'comment' | 'expiration'>;
 export type OmitMetadata<T> = Omit<T, 'owner' | 'comment' | 'expiration'>;
 
 export type ClassifiedEvent<T extends IGDPRPropertyWithoutMetadata> = {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [k in keyof T]: any;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    [k in keyof T]: any;
 };
 
 export type StrictPropertyChecker<TEvent, TClassification, TError> =
-  keyof TEvent extends keyof OmitMetadata<TClassification>
-    ? keyof OmitMetadata<TClassification> extends keyof TEvent
-      ? TEvent
-      : TError
-    : TError;
+    keyof TEvent extends keyof OmitMetadata<TClassification>
+        ? keyof OmitMetadata<TClassification> extends keyof TEvent
+            ? TEvent
+            : TError
+        : TError;
 
 export type StrictPropertyCheckError = { error: 'Type of classified event does not match event properties' };
 
 export type StrictPropertyCheck<T extends IGDPRProperty, E> = StrictPropertyChecker<
-  E,
-  ClassifiedEvent<OmitMetadata<T>>,
-  StrictPropertyCheckError
+    E,
+    ClassifiedEvent<OmitMetadata<T>>,
+    StrictPropertyCheckError
 >;
 
 let telemetryReporter: TelemetryReporter;
@@ -70,160 +70,163 @@ let telemetryReporter: TelemetryReporter;
  * Which must be awaited before sending the telemetry.
  */
 export function publicLog2<E extends ClassifiedEvent<OmitMetadata<T>> = never, T extends IGDPRProperty = never>(
-  eventName: string,
-  data?: StrictPropertyCheck<T, E>
+    eventName: string,
+    data?: StrictPropertyCheck<T, E>
 ) {
-  telemetryReporter = telemetryReporter
-    ? telemetryReporter
-    : disposableStore.add(new TelemetryReporter(AppInsightsKey));
-  telemetryReporter.sendTelemetryEvent(eventName, data);
+    telemetryReporter = telemetryReporter
+        ? telemetryReporter
+        : disposableStore.add(new TelemetryReporter(AppInsightsKey));
+    telemetryReporter.sendTelemetryEvent(eventName, data);
 }
 
 interface TensorboardEntrypontTriggeredData {
-  trigger: TensorBoardEntrypointTrigger;
-  entrypoint: TensorBoardEntrypoint;
+    trigger: TensorBoardEntrypointTrigger;
+    entrypoint: TensorBoardEntrypoint;
 }
 type TensorboardEntrypontTriggeredDataClassification = {
-  owner: 'donjayamanne';
-  comment: 'Codelens displayed';
-  trigger: {
-    classification: 'SystemMetaData';
-    purpose: 'FeatureInsight';
-    comment: 'Location where codelens was displayed';
-  };
-  entrypoint: {
-    classification: 'SystemMetaData';
-    purpose: 'FeatureInsight';
-    comment: 'Code lens displayed';
-  };
+    owner: 'donjayamanne';
+    comment: 'Codelens displayed';
+    trigger: {
+        classification: 'SystemMetaData';
+        purpose: 'FeatureInsight';
+        comment: 'Location where codelens was displayed';
+    };
+    entrypoint: {
+        classification: 'SystemMetaData';
+        purpose: 'FeatureInsight';
+        comment: 'Code lens displayed';
+    };
 };
 /**
  * Safe way to send data in telemetry (obfuscate PII).
  */
 export async function getTelemetrySafeHashedString(data: string) {
-  return computeHash(data, 'SHA-256');
+    return computeHash(data, 'SHA-256');
 }
 
 export function sendTensorboardEntrypointTriggered(
-  trigger: TensorBoardEntrypointTrigger,
-  entrypoint: TensorBoardEntrypoint
+    trigger: TensorBoardEntrypointTrigger,
+    entrypoint: TensorBoardEntrypoint
 ) {
-  publicLog2<TensorboardEntrypontTriggeredData, TensorboardEntrypontTriggeredDataClassification>(
-    'TENSORBOARD.ENTRYPOINT_SHOWN',
-    {
-      trigger,
-      entrypoint,
-    }
-  );
+    publicLog2<TensorboardEntrypontTriggeredData, TensorboardEntrypontTriggeredDataClassification>(
+        'TENSORBOARD.ENTRYPOINT_SHOWN',
+        {
+            trigger,
+            entrypoint
+        }
+    );
 }
 
 interface TensorboardSelectionData {
-  selection: TensorBoardPromptSelection;
+    selection: TensorBoardPromptSelection;
 }
 type TensorboardSelectionDataClassification = {
-  owner: 'donjayamanne';
-  comment: 'Tensorboard prompt';
-  selection: {
-    classification: 'SystemMetaData';
-    purpose: 'FeatureInsight';
-    comment: 'Selection from the prompt';
-  };
+    owner: 'donjayamanne';
+    comment: 'Tensorboard prompt';
+    selection: {
+        classification: 'SystemMetaData';
+        purpose: 'FeatureInsight';
+        comment: 'Selection from the prompt';
+    };
 };
 export function sendTensorboardPromptSelection(selection: TensorBoardPromptSelection) {
-  publicLog2<TensorboardSelectionData, TensorboardSelectionDataClassification>('TENSORBOARD.INSTALL_PROMPT_SELECTION', {
-    selection,
-  });
+    publicLog2<TensorboardSelectionData, TensorboardSelectionDataClassification>(
+        'TENSORBOARD.INSTALL_PROMPT_SELECTION',
+        {
+            selection
+        }
+    );
 }
 interface TensorboardLaunchData {
-  entrypoint: TensorBoardEntrypoint;
-  trigger: TensorBoardEntrypointTrigger;
+    entrypoint: TensorBoardEntrypoint;
+    trigger: TensorBoardEntrypointTrigger;
 }
 type TensorboardLaunchDataClassification = {
-  owner: 'donjayamanne';
-  comment: 'Tensorboard prompt';
-  trigger: {
-    classification: 'SystemMetaData';
-    purpose: 'FeatureInsight';
-    comment: 'Location where codelens was displayed';
-  };
-  entrypoint: {
-    classification: 'SystemMetaData';
-    purpose: 'FeatureInsight';
-    comment: 'Code lens displayed';
-  };
+    owner: 'donjayamanne';
+    comment: 'Tensorboard prompt';
+    trigger: {
+        classification: 'SystemMetaData';
+        purpose: 'FeatureInsight';
+        comment: 'Location where codelens was displayed';
+    };
+    entrypoint: {
+        classification: 'SystemMetaData';
+        purpose: 'FeatureInsight';
+        comment: 'Code lens displayed';
+    };
 };
 export function sendTensorboardLaunch(entrypoint: TensorBoardEntrypoint, trigger: TensorBoardEntrypointTrigger) {
-  publicLog2<TensorboardLaunchData, TensorboardLaunchDataClassification>('TENSORBOARD.SESSION_LAUNCH', {
-    trigger,
-    entrypoint,
-  });
+    publicLog2<TensorboardLaunchData, TensorboardLaunchDataClassification>('TENSORBOARD.SESSION_LAUNCH', {
+        trigger,
+        entrypoint
+    });
 }
 
 interface JumptToSourceData {}
 type JumptToSourceDataClassification = {
-  owner: 'donjayamanne';
-  comment: 'Tensorboard jump to source';
+    owner: 'donjayamanne';
+    comment: 'Tensorboard jump to source';
 };
 export function sendJumptToSource() {
-  publicLog2<JumptToSourceData, JumptToSourceDataClassification>('TENSORBOARD_JUMP_TO_SOURCE_REQUEST');
+    publicLog2<JumptToSourceData, JumptToSourceDataClassification>('TENSORBOARD_JUMP_TO_SOURCE_REQUEST');
 }
 interface JumptToSourceSourceNotFoundData {}
 type JumptToSourceSourceNotFoundDataClassification = {
-  owner: 'donjayamanne';
-  comment: 'Tensorboard jump to source';
+    owner: 'donjayamanne';
+    comment: 'Tensorboard jump to source';
 };
 export function sendJumptToSourceNotFound() {
-  publicLog2<JumptToSourceSourceNotFoundData, JumptToSourceSourceNotFoundDataClassification>(
-    'TENSORBOARD_JUMP_TO_SOURCE_FILE_NOT_FOUND'
-  );
+    publicLog2<JumptToSourceSourceNotFoundData, JumptToSourceSourceNotFoundDataClassification>(
+        'TENSORBOARD_JUMP_TO_SOURCE_FILE_NOT_FOUND'
+    );
 }
 
 interface TensorboardDetectedInTerminalFoundData {}
 type TensorboardDetectedInTerminalDataClassification = {
-  owner: 'donjayamanne';
-  comment: 'Tensorboard jump to source';
+    owner: 'donjayamanne';
+    comment: 'Tensorboard jump to source';
 };
 export function sendTensorboardDetectedInTerminal() {
-  publicLog2<TensorboardDetectedInTerminalFoundData, TensorboardDetectedInTerminalDataClassification>(
-    'TENSORBOARD_JUMP_TO_SOURCE_FILE_NOT_FOUND'
-  );
+    publicLog2<TensorboardDetectedInTerminalFoundData, TensorboardDetectedInTerminalDataClassification>(
+        'TENSORBOARD_JUMP_TO_SOURCE_FILE_NOT_FOUND'
+    );
 }
 
 interface TensorboardStartupData {
-  result: TensorBoardSessionStartResult;
-  duration: number;
+    result: TensorBoardSessionStartResult;
+    duration: number;
 }
 type TensorboardStartupDataClassification = {
-  owner: 'donjayamanne';
-  comment: 'Codelens displayed';
-  duration: {
-    classification: 'SystemMetaData';
-    purpose: 'FeatureInsight';
-    comment: 'Duration to start tensorboard';
-    isMeasurement: true;
-  };
-  result: {
-    classification: 'SystemMetaData';
-    purpose: 'FeatureInsight';
-    comment: 'Result of starting tensorboard';
-  };
+    owner: 'donjayamanne';
+    comment: 'Codelens displayed';
+    duration: {
+        classification: 'SystemMetaData';
+        purpose: 'FeatureInsight';
+        comment: 'Duration to start tensorboard';
+        isMeasurement: true;
+    };
+    result: {
+        classification: 'SystemMetaData';
+        purpose: 'FeatureInsight';
+        comment: 'Result of starting tensorboard';
+    };
 };
 export function sendTensorboardStartupResult(duration: number, result: TensorBoardSessionStartResult) {
-  publicLog2<TensorboardStartupData, TensorboardStartupDataClassification>(
-    'TENSORBOARD.SESSION_DAEMON_STARTUP_DURATION',
-    {
-      duration,
-      result,
-    }
-  );
+    publicLog2<TensorboardStartupData, TensorboardStartupDataClassification>(
+        'TENSORBOARD.SESSION_DAEMON_STARTUP_DURATION',
+        {
+            duration,
+            result
+        }
+    );
 }
 
 interface TensorboardUsage {}
 type TensorboardUsageClassification = {
-  owner: 'donjayamanne';
-  comment: 'Sent extension activates';
+    owner: 'donjayamanne';
+    comment: 'Sent extension activates';
 };
 
 export function trackInstallOfExtension() {
-  publicLog2<TensorboardUsage, TensorboardUsageClassification>('activated', {});
+    publicLog2<TensorboardUsage, TensorboardUsageClassification>('activated', {});
 }
